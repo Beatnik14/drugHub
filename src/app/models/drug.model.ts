eb13ad0f15bed5ae5a7e356928df057e86b18a79
isebiskveradze <i.sebiskveradze@gmail.com>
2020-12-19 21:18:41 +0400

    adddrugmodel
    drugmodeladd

--- src/app/models/drug.model.ts
+++ src/app/models/drug.model.ts
@@ -1,4 +1,5 @@
 export class DrugModel {
+    id?;
     name;
     date;
     type;
@@ -6,12 +7,13 @@ export class DrugModel {
     description;
     mobileNumber
 
-    constructor(name, date, type, quantity, description, mobileNumber) {
+    constructor(name, date, type, quantity, description, mobileNumber,id?) {
         this.name = name;
         this.date = date;
         this.type = type;
         this.quantity = quantity;
         this.description = description;
         this.mobileNumber = mobileNumber;
+        this.id = id
     }
 }
