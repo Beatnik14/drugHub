bf5d62fa33411b18cdd73f96ace469531b2cd052
Beatnik14 <sabatevdorashvili99@gmail.com>
2020-12-20 18:08:36 +0400

    drug detail

--- src/app/drug-detail/drug-detail.component.ts
+++ src/app/drug-detail/drug-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { DrugServiceService } from '../services/drug-service.service';
 
 @Component({
   selector: 'app-drug-detail',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drug-detail.component.css']
 })
 export class DrugDetailComponent implements OnInit {
-
-  constructor() { }
+  drug;
+  drugs = [];
+  drugFromApi;
+  constructor(private route: ActivatedRoute, private drugService: DrugServiceService) { }
 
   ngOnInit(): void {
+    this.drug = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name'],
+    }
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.drug.id = params['id'];
+          this.drug.name = params['name'];
+        }
+      )
+    this.drugService.getDrugs()
+      .subscribe((data) => {
+        this.drugs = data
+        for (let index = 0; index < data.length; index++) {
+
+          if (this.drugs[index].name == this.drug.name) {
+            this.drugFromApi = this.drugs[index]
+          }
+        }
+        console.log(this.drugFromApi)
+      })
+    
   }
 
 }
